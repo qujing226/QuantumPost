fc2fa68a572ac6156b8a3d9b511a43e2fc2f9a0b
qujing226 <qujing226@gmail.com>
2025-03-09 19:19:52 +0800

    refactor

---
+++ sms&mail.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE sms_mail;
+USE sms_mail;
+
+create table biz (
+    id int not null auto_increment,
+
+    name      varchar(255) not null,
+    applicant varchar(255) not null,
+    applicantId int not null,
+    status      int not null default 0,
+
+
+    sms_cap int not null,
+    mail_cap int not null,
+    sms_num int,
+    mail_num int,
+    primary key (id)
+);
