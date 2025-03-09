72ffed96fb89d9282787ca18181c6c7a87da4091
qujing226 <qujing226@gmail.com>
2025-03-09 19:24:36 +0800

    new

--- sms&mail.sql
+++ sms&mail.sql
@@ -9,10 +9,20 @@ create table biz (
     applicantId int not null,
     status      int not null default 0,
 
-
     sms_cap int not null,
     mail_cap int not null,
     sms_num int,
     mail_num int,
+    primary key (id)
+);
+
+create table sms (
+    id int not null auto_increment,
+    bizId int not null,
+    phone varchar(255) not null,
+    content varchar(255) not null,
+
+
+    status int not null default 0,
     primary key (id)
 );
